7782fe0860cd285e071bfa8cdb5fc0f0fdd0c33d
truongphamexpogain <Truong.Pham@expogain.com>
2025-04-16 19:06:57 -0400

    commit all

--- Lab04/LabWebApp.Tests/obj/Debug/net8.0/LabWebApp.Tests.AssemblyInfo.cs
+++ Lab04/LabWebApp.Tests/obj/Debug/net8.0/LabWebApp.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabWebApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21fcc5bf475e85be12b36ea4cc5c4db4616b217f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabWebApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabWebApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
